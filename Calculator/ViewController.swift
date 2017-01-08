f522f35078511f9d8bedc55b456310f09160bca2
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-01-08 00:00:19 -0500

    Updates on Refactored Code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -51,8 +51,53 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operate (sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userentersNumbers {
+            enter()
+        }
+        
+        switch operation {
+        case "+":performOperation {$0 + $1}
+        case "−":performOperation {$1 - $0}
+        case "×":performOperation {$0 * $1}
+        case "÷":performOperation {$0 / $1}
+        case "√":performOperation {sqrt($0)}
+        default: break
+        }
+    }
     
+    func performOperation (operation: (Double) -> Double) {
+        if operandStank.count >= 2 {
+            displayvalue = operation (operandStank.removeLast(), operandStank.removeLast())
+            
+            enter()
+        }
     }
 
+    func performOperation (operation: (Double) -> Double) {
+        if operandStank.count >= 1 {
+            displayvalue = operation (operandStank.removeLast())
+            
+            enter()
+        }
+    }
+    
+    func add (op1: Double, op2: Double) -> Double {
+        return op1 + op2
+    }
+    
+    func subtract (op1: Double, op2: Double) -> Double {
+        return op1 - op2
+    }
+    
+    func multiply (op1: Double, op2: Double) -> Double {
+        return op1 * op2
+    }
+    
+    func divide (op1: Double, op2: Double) -> Double {
+        return op1 / op2
+    }
 
+}
 
